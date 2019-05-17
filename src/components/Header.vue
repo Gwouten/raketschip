ed11c447b1cf86de4ca85570426df1771ad46282
Wouter Deloof <Gwouten@Wouters-iMac-2.local>
2019-05-17 13:01:54 +0200

    Added font, Header component

---
+++ src/components/Header.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class=''>
+        <h1>Raketschip</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang='scss'>
+    h1 {
+        color: $primary;
+        font-family: $font-header;
+        text-transform: uppercase;
+    }
+</style>
---
+++ src/mixins/dato-cms.js
@@ -0,0 +1,23 @@
+// const token = 'ba0473327634af2c896b1293615902';
+
+// fetch(
+//   'https://graphql.datocms.com/',
+//   {
+//     method: 'POST',
+//     headers: {
+//       'Content-Type': 'application/json',
+//       'Accept': 'application/json',
+//       'Authorization': `Bearer ${token}`,
+//     },
+//     body: JSON.stringify({
+//       query: '{ allPosts { title } }'
+//     }),
+//   }
+// )
+// .then(res => res.json())
+// .then((res) => {
+//   console.log(res.data)
+// })
+// .catch((error) => {
+//   console.log(error);
+// });
